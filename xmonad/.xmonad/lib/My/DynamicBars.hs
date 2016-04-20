5c78d200c9f75b3385da8fd04aaf3a07f3a89844
seanstrom <sean@seanhagstrom.com>
2016-04-19 18:55:21 -0700

    update port of DynamicBars module

--- xmonad/.xmonad/lib/My/DynamicBars.hs
+++ xmonad/.xmonad/lib/My/DynamicBars.hs
@@ -1,22 +1,38 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  XMonad.Hooks.DynamicBars
+-- Copyright   :  (c) Ben Boeckel 2012
+-- License     :  BSD-style (as xmonad)
+--
+-- Maintainer  :  mathstuf@gmail.com
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+-- Manage per-screen status bars.
+--
+-----------------------------------------------------------------------------
+
 module My.DynamicBars (
-  DynamicStatusBar
+  -- * Usage
+  -- $usage
+    DynamicStatusBar
   , DynamicStatusBarCleanup
   , dynStatusBarStartup
   , dynStatusBarEventHook
   , multiPP
-  , multiPPWithLog
+  , multiPPFormat
   ) where
 
 import Prelude
 
-import Control.Concurrent.MVar
 import Control.Monad
 import Control.Monad.Trans (lift)
 import Control.Monad.Writer (WriterT, execWriterT, tell)
 
 import Data.Maybe
 import Data.Monoid
-import Data.Traversable (traverse)
+import Data.Foldable (traverse_)
 
 import Graphics.X11.Xinerama
 import Graphics.X11.Xlib
@@ -24,46 +40,66 @@ import Graphics.X11.Xlib.Extras
 import Graphics.X11.Xrandr
 
 import System.IO
-import System.IO.Unsafe
 
 import XMonad
 import qualified XMonad.StackSet as W
 import XMonad.Hooks.DynamicLog
+import qualified XMonad.Util.ExtensibleState as XS
+
+-- $usage
+-- Provides a few helper functions to manage per-screen status bars while
+-- dynamically responding to screen changes. A startup action, event hook, and
+-- a way to separate PP styles based on the screen's focus are provided:
+--
+-- * The 'dynStatusBarStartup' hook which initializes the status bars.
+--
+-- * The 'dynStatusBarEventHook' hook which respawns status bars when the
+-- number of screens changes.
+--
+-- * The 'multiPP' function which allows for different output based on whether
+-- the screen for the status bar has focus.
+--
+-- * The 'multiPPFormat' function is the same as the 'multiPP' function, but it
+-- also takes in a function that can customize the output to status bars.
+--
+-- The hooks take a 'DynamicStatusBar' function which is given the id of the
+-- screen to start up and returns the 'Handle' to the pipe to write to. The
+-- 'DynamicStatusBarCleanup' argument should tear down previous instances. It
+-- is called when the number of screens changes and on startup.
+--
 
 data DynStatusBarInfo = DynStatusBarInfo
   { dsbInfoScreens :: [ScreenId]
   , dsbInfoHandles :: [Handle]
-  }
+  } deriving (Typeable)
+
+instance ExtensionClass DynStatusBarInfo where
+  initialValue = DynStatusBarInfo [] []
 
 type DynamicStatusBar = ScreenId -> IO Handle
 type DynamicStatusBarCleanup = IO ()
 
--- Global state
-statusBarInfo :: MVar DynStatusBarInfo
-statusBarInfo = unsafePerformIO $ newMVar (DynStatusBarInfo [] [])
-
 dynStatusBarStartup :: DynamicStatusBar -> DynamicStatusBarCleanup -> X ()
-dynStatusBarStartup sb cleanup = liftIO $ do
-  dpy <- openDisplay ""
-  xrrSelectInput dpy (defaultRootWindow dpy) rrScreenChangeNotifyMask
-  closeDisplay dpy
+dynStatusBarStartup sb cleanup = do
+  dpy <- asks display
+  root <- asks theRoot
+  io $ xrrSelectInput dpy root rrScreenChangeNotifyMask
   updateStatusBars sb cleanup
 
 dynStatusBarEventHook :: DynamicStatusBar -> DynamicStatusBarCleanup -> Event -> X All
-dynStatusBarEventHook sb cleanup (RRScreenChangeNotifyEvent {}) = liftIO (updateStatusBars sb cleanup) >> return (All True)
+dynStatusBarEventHook sb cleanup (RRScreenChangeNotifyEvent {}) = updateStatusBars sb cleanup >> return (All True)
 dynStatusBarEventHook _  _       _                              = return (All True)
 
-updateStatusBars :: DynamicStatusBar -> DynamicStatusBarCleanup -> IO ()
-updateStatusBars sb cleanup = liftIO $ do
-  dsbInfo <- takeMVar statusBarInfo
+updateStatusBars :: DynamicStatusBar -> DynamicStatusBarCleanup -> X ()
+updateStatusBars sb cleanup = do
+  dsbInfo <- XS.get
   screens <- getScreens
-  if (screens /= (dsbInfoScreens dsbInfo))
-    then do
-      mapM hClose (dsbInfoHandles dsbInfo)
-      cleanup
-      newHandles <- mapM sb screens
-      putMVar statusBarInfo (DynStatusBarInfo screens newHandles)
-    else putMVar statusBarInfo dsbInfo
+  when (screens /= dsbInfoScreens dsbInfo) $ do
+      newHandles <- liftIO $ do
+          hClose `mapM_` dsbInfoHandles dsbInfo
+          cleanup
+          mapM sb screens
+      XS.put $ DynStatusBarInfo screens newHandles
 
 -----------------------------------------------------------------------------
 -- The following code is from adamvo's xmonad.hs file.
@@ -72,13 +108,11 @@ updateStatusBars sb cleanup = liftIO $ do
 multiPP :: PP -- ^ The PP to use if the screen is focused
         -> PP -- ^ The PP to use otherwise
         -> X ()
-multiPP focusPP unfocusPP = do
-  dsbInfo <- liftIO $ readMVar statusBarInfo
-  multiPP' dynamicLogString focusPP unfocusPP (dsbInfoHandles dsbInfo)
+multiPP = multiPPFormat dynamicLogString
 
-multiPPWithLog :: (PP -> X String) -> PP -> PP -> X ()
-multiPPWithLog dynlStr focusPP unfocusPP = do
-  dsbInfo <- liftIO $ readMVar statusBarInfo
+multiPPFormat :: (PP -> X String) -> PP -> PP -> X ()
+multiPPFormat dynlStr focusPP unfocusPP = do
+  dsbInfo <- XS.get
   multiPP' dynlStr focusPP unfocusPP (dsbInfoHandles dsbInfo)
 
 multiPP' :: (PP -> X String) -> PP -> PP -> [Handle] -> X ()
@@ -91,13 +125,12 @@ multiPP' dynlStr focusPP unfocusPP handles = do
         out <- lift $ dynlStr $ if isFoc then focusPP else unfocusPP
         when isFoc $ get >>= tell . Last . Just
         return out
-  traverse put . getLast
+  traverse_ put . getLast
     =<< execWriterT . (io . zipWithM_ hPutStrLn handles <=< mapM pickPP) . catMaybes
     =<< mapM screenWorkspace (zipWith const [0 .. ] handles)
-  return ()
 
-getScreens :: IO [ScreenId]
-getScreens = do
+getScreens :: MonadIO m => m [ScreenId]
+getScreens = liftIO $ do
   screens <- do
     dpy <- openDisplay ""
     rects <- getScreenInfo dpy
